cd0608daaf84bc7901a98fc63c6f251051948744
layule <dbf6655@naver.com>
2023-03-30 15:09:05 +0900

    랴: 혁균이가 베스트셀러 마무리해줌ㅋ

--- src/pages/Bestseller.js
+++ src/pages/Bestseller.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import * as jQuery from "jquery"
 import Swiper from "swiper"
+import { bsFavicon, sellerProfile1, sellerProfile2, sellerProfile3, sellerProfile4, sellerUi1, sellerUi2, sellerUi3, sellerUi4 } from "../images/bestseller"
 
 export default function Component() {
   const scrollToGiveClass = () => {
@@ -187,39 +188,12 @@ removeFunction : function(){}
 
   return (
     <>
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Bestseller</title>
+      {/* <!-- favicon--> */}
+      {/* <link rel="shortcut icon" href={bsFavicon} /> */}
+      {/* <!-- 플러그인 연결 --> */}
+      <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
 
-        {/* <!-- favicon--> */}
-        <link rel="shortcut icon" href="images/bs_favicon.png" />
-
-        {/* <!-- css --> */}
-        <link rel="stylesheet" href="../src/style.css" />
-        <link rel="stylesheet" href="../src/stylem.css" />
-
-        {/* <!-- reset css--> */}
-        <link rel="stylesheet" href="../src/reset.css" />
-
-        {/* <!-- font --> */}
-        <link rel="stylesheet" href="../src/font.css" />
-
-        {/* <!-- jQuery --> */}
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="js/jquery.scrollToGiveClass.js"></script>
-
-        {/* <!-- 플러그인 연결 --> */}
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
-        />
-
-        <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-        {/* <!-- js 호출 --> */}
-        <script src="js/script.js"></script>
-      </head>
+      <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
       <body>
         <nav>
           <ul id="nav">
@@ -350,12 +324,8 @@ removeFunction : function(){}
                 <a href="#" className="seller_btn">
                   <span>1분컷 사용법</span>
                 </a>
-                <p className="seller_txt">
-                  *it-마켓 : 베스트셀러만의 특가 판매 혜택
-                </p>
-                <p className="seller_txthighlight">
-                  실제 셀러의 판매 금액입니다.
-                </p>
+                <p className="seller_txt">*it-마켓 : 베스트셀러만의 특가 판매 혜택</p>
+                <p className="seller_txthighlight">실제 셀러의 판매 금액입니다.</p>
               </div>
               {/* <!-- .txt_group --> */}
 
@@ -433,7 +403,7 @@ removeFunction : function(){}
                     영양제가 좋아서 쓴<br />찐 리뷰가 대박났어요!
                   </p>
                   <div className="card-profile">
-                    <img src="images/seller_profile1.png" alt="review image" />
+                    <img src={sellerProfile1} alt="review image" />
                     <p>여행블로거 | 이웃 773명</p>
                   </div>
                 </div>
@@ -447,17 +417,9 @@ removeFunction : function(){}
                       <span>판매액 12,040,000원</span>
                     </p>
                     <p className="comment">제품가 4만원 x 43일 x 하루 7건</p>
-                    <p className="content">
-                      블로그 글쓰기를 좋아해서 쓰고 효과 본 영양제 리뷰를 판매
-                      링크와 함께 올렸더니 문의량 주문이 너무 많이 들어와서 깜짝
-                      놀랐어요!
-                    </p>
+                    <p className="content">블로그 글쓰기를 좋아해서 쓰고 효과 본 영양제 리뷰를 판매 링크와 함께 올렸더니 문의량 주문이 너무 많이 들어와서 깜짝 놀랐어요!</p>
                   </div>
-                  <img
-                    className="ui-img"
-                    src="images/seller_ui1.png"
-                    alt="review image"
-                  />
+                  <img className="ui-img" src={sellerUi1} alt="review image" />
                 </div>
                 {/* <!-- .card-hover --> */}
               </div>
@@ -473,7 +435,7 @@ removeFunction : function(){}
                     수익을 벌었어요.
                   </p>
                   <div className="card-profile">
-                    <img src="images/seller_profile2.png" alt="review image" />
+                    <img src={sellerProfile2} alt="review image" />
                     <p>육아맘 다이어터 | 팔로우 15,999명</p>
                   </div>
                 </div>
@@ -486,20 +448,12 @@ removeFunction : function(){}
                       <span>건강한 시리얼</span>
                       <span>판매액 4,298,300원</span>
                     </p>
-                    <p className="comment">
-                      제품가 26,000원 x 일주일동안 162개
-                    </p>
+                    <p className="comment">제품가 26,000원 x 일주일동안 162개</p>
                     <p className="content">
-                      어렵게 살을 뺴면서 알게 된 다이어트팁과 좋은 식단 아이템을
-                      추천 할 수 있었어요. 정말 사랑하는 상품을 추천해서 더
-                      반응도 좋았던 거 같아요.
+                      어렵게 살을 뺴면서 알게 된 다이어트팁과 좋은 식단 아이템을 추천 할 수 있었어요. 정말 사랑하는 상품을 추천해서 더 반응도 좋았던 거 같아요.
                     </p>
                   </div>
-                  <img
-                    className="ui-img"
-                    src="images/seller_ui2.png"
-                    alt="review image"
-                  />
+                  <img className="ui-img" src={sellerUi2} alt="review image" />
                 </div>
                 {/* <!-- .card-hover --> */}
               </div>
@@ -515,7 +469,7 @@ removeFunction : function(){}
                     리뷰했어요!
                   </p>
                   <div className="card-profile">
-                    <img src="images/seller_profile3.png" alt="review image" />
+                    <img src={sellerProfile3} alt="review image" />
                     <p>맘&뷰티 인플러언서 | 팔로워 22,255명</p>
                   </div>
                 </div>
@@ -530,16 +484,10 @@ removeFunction : function(){}
                     </p>
                     <p className="comment">제품가 4만원 x 43일 x 하루 7건</p>
                     <p className="content">
-                      자주 얼굴이 부어서 붓기템을 많이 사봤어요! 마침 협찬
-                      제안을 주셔서 먹어봤더니 효과가 좋아서 비포&애프터로 리뷰
-                      올렸더니 인친들이 구매문의를 많이 하시더라구요!
+                      자주 얼굴이 부어서 붓기템을 많이 사봤어요! 마침 협찬 제안을 주셔서 먹어봤더니 효과가 좋아서 비포&애프터로 리뷰 올렸더니 인친들이 구매문의를 많이 하시더라구요!
                     </p>
                   </div>
-                  <img
-                    className="ui-img"
-                    src="images/seller_ui3.png"
-                    alt="review image"
-                  />
+                  <img className="ui-img" src={sellerUi3} alt="review image" />
                 </div>
                 {/* <!-- .card-hover --> */}
               </div>
@@ -555,7 +503,7 @@ removeFunction : function(){}
                     인친에게 소개했어요!
                   </p>
                   <div className="card-profile">
-                    <img src="images/seller_profile4.png" alt="review image" />
+                    <img src={sellerProfile4} alt="review image" />
                     <p>푸드 인플루언서 | 팔로우 11,149명</p>
                   </div>
                 </div>
@@ -569,17 +517,9 @@ removeFunction : function(){}
                       <span>판매액 5,677,900원</span>
                     </p>
                     <p className="comment">제품가 21,506원 x 5일 동안 264건</p>
-                    <p className="content">
-                      새로운 음식을 인친에게 소개하는 걸 좋아했는데, 평소 즐겨
-                      먹던 미주라를 피드에서 최저가로 소개하고 수익도 벌어서
-                      즐거웠어요!
-                    </p>
+                    <p className="content">새로운 음식을 인친에게 소개하는 걸 좋아했는데, 평소 즐겨 먹던 미주라를 피드에서 최저가로 소개하고 수익도 벌어서 즐거웠어요!</p>
                   </div>
-                  <img
-                    className="ui-img"
-                    src="images/seller_ui4.png"
-                    alt="review image"
-                  />
+                  <img className="ui-img" src={sellerUi4} alt="review image" />
                 </div>
                 {/* <!-- .card-hover --> */}
               </div>
@@ -599,20 +539,15 @@ removeFunction : function(){}
                     진행 프로세스가 궁금합니다.
                   </h3>
                   <div className="faq-content">
-                    {
-                      "[조건 협의] -> [입점 계약] -> [상품 등록] -> [셀러 신청] -> [공구 진행] -> [정산]"
-                    }
+                    {"[조건 협의] -> [입점 계약] -> [상품 등록] -> [셀러 신청] -> [공구 진행] -> [정산]"}
                     <br />
                     진행 상품, 판매가, 공급가, 수량 등 조건 협의를 합니다.
                     <br />
-                    양사 간 협의 후 계약서 이견 없으시면 전자 계약서를 발송
-                    드립니다.
+                    양사 간 협의 후 계약서 이견 없으시면 전자 계약서를 발송 드립니다.
                     <br />
-                    계약일로부터 약 7일 이내 사이트에 등록 후 셀러 신청을
-                    받습니다.
+                    계약일로부터 약 7일 이내 사이트에 등록 후 셀러 신청을 받습니다.
                     <br />
-                    공구를 희망하는 셀러와 공구 일정을 잡고 해당일에 판매를
-                    진행합니다.
+                    공구를 희망하는 셀러와 공구 일정을 잡고 해당일에 판매를 진행합니다.
                   </div>
                   {/* <!--//.faq-content--> */}
                 </li>
@@ -624,11 +559,9 @@ removeFunction : function(){}
                   <div className="faq-content">
                     수수료는 운영비가 포함된 금액이며,
                     <br />
-                    수수료는 동일하게 진행하여 수수료가 높을수록 셀러들에게
-                    지급되는
+                    수수료는 동일하게 진행하여 수수료가 높을수록 셀러들에게 지급되는
                     <br />
-                    수수료가 높아지기 때문에 참여율이 높아진다는 점 참고
-                    부탁드립니다.
+                    수수료가 높아지기 때문에 참여율이 높아진다는 점 참고 부탁드립니다.
                   </div>
                   {/* <!--//.faq-content--> */}
                 </li>
@@ -640,9 +573,7 @@ removeFunction : function(){}
                   <div className="faq-content">
                     본 사이트는 폐쇄율이라 온라인 검색 시 노출되지않습니다.
                     <br />
-                    특정 기간 동안 저렴한 가격으로 판매하는 공구 특성상 온라인
-                    검색 시 노출되는 최저가 보다 저렴한 금액을 진행하고
-                    있습니다.
+                    특정 기간 동안 저렴한 가격으로 판매하는 공구 특성상 온라인 검색 시 노출되는 최저가 보다 저렴한 금액을 진행하고 있습니다.
                   </div>
                   {/* <!--//.faq-content--> */}
                 </li>
@@ -652,8 +583,7 @@ removeFunction : function(){}
                     몰에 등록되면 바로 셀러가 판매하는 건가요?
                   </h3>
                   <div className="faq-content">
-                    판매를 희망하는 셀러가 있으면 바로 가능하며, 기간이 더
-                    소요될 수도 있습니다.
+                    판매를 희망하는 셀러가 있으면 바로 가능하며, 기간이 더 소요될 수도 있습니다.
                     <br />
                     상품에 따라 상이한 점 참고 부탁드립니다.
                   </div>
@@ -669,8 +599,7 @@ removeFunction : function(){}
                     셀러를 선택할 수 있나요?
                   </h3>
                   <div className="faq-content">
-                    브랜드의 니즈 및 상황에 맞춘 다양한 서비스 모델을 제공하고
-                    있습니다.
+                    브랜드의 니즈 및 상황에 맞춘 다양한 서비스 모델을 제공하고 있습니다.
                     <br />
                     서비스에 따라 추가 비용이 발생할 수 있습니다.
                   </div>
@@ -682,11 +611,9 @@ removeFunction : function(){}
                     배송은 어떻게 하나요?
                   </h3>
                   <div className="faq-content">
-                    주문서를 취함하여 업체 발주 마감 시간 전 발주 메일로 전달
-                    드리고 있습니다.
+                    주문서를 취함하여 업체 발주 마감 시간 전 발주 메일로 전달 드리고 있습니다.
                     <br />
-                    업체에서 출고 후 송장 번호 회신 주시면 송장 등록하고
-                    있습니다.
+                    업체에서 출고 후 송장 번호 회신 주시면 송장 등록하고 있습니다.
                   </div>
                   {/* <!--//.faq-content--> */}
                 </li>
@@ -695,13 +622,11 @@ removeFunction : function(){}
                     <span>Q</span>몇 명의 셀러가 진행하나요?
                   </h3>
                   <div className="faq-content">
-                    소비자 누구나 판매할 수 있도록 돕는 플랫폼이기 때문에
-                    진행하는
+                    소비자 누구나 판매할 수 있도록 돕는 플랫폼이기 때문에 진행하는
                     <br />
                     셀러의 제한을 두지 않습니다.
                     <br />
-                    원하시는 조건이 있으실 경우 사전에 말씀해주시면 협의
-                    가능합니다.
+                    원하시는 조건이 있으실 경우 사전에 말씀해주시면 협의 가능합니다.
                   </div>
                   {/* <!--//.faq-content--> */}
                 </li>
@@ -740,9 +665,7 @@ removeFunction : function(){}
                 <span>사업자등록번호:123-45-67890</span>
                 <span>통신판매업신고:제2022-서울서초-4251호</span>
                 <span>대표:김라율</span>
-                <span>
-                  주소:서울특별시 서초구 강남대로 25길 33 남경빌딩 4층
-                </span>
+                <span>주소:서울특별시 서초구 강남대로 25길 33 남경빌딩 4층</span>
                 <br />
                 <span>E-MAIL:dbf6655@naver.com</span>
               </p>
@@ -757,9 +680,7 @@ removeFunction : function(){}
                 </a>
               </div>
 
-              <p className="copyright">
-                2022 &copy; bestseller corp. All right reserved.
-              </p>
+              <p className="copyright">2022 &copy; bestseller corp. All right reserved.</p>
             </div>
             {/* <!-- .footer-info --> */}
           </div>
