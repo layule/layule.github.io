2bf6c4bdcf550fc8d11761c8bacdda4272d86bce
layule <dbf6655@naver.com>
2023-04-13 11:05:57 +0900

    fix: 르라보 수정

--- src/pages/lelabo.js
+++ src/pages/lelabo.js
@@ -1,12 +1,11 @@
 import * as React from "react"
 import * as jQuery from "jquery"
 import Swiper from "swiper"
-import { logoLabo, newIn, pr10, pr11, pr12, pr13, pr14, pr15, pr16, pr17, pr18, pr19, pr4, pr5, pr6, pr7, pr8, pr9 } from "../images/lelabo"
+import { logoLabo, newIn, pr10, pr11, pr12, pr13, pr14, pr15, pr16, pr17, pr18, pr19, pr1L, pr2L, pr3, pr4, pr5, pr6, pr7, pr8, pr9 } from "../images/lelabo"
 import "/src/styles/lelabo/font.css"
 import "/src/styles/lelabo/reset.css"
 import "/src/styles/lelabo/style.css"
 import "/src/styles/lelabo/stylem.css"
-import { logo, pr1, pr2, pr3 } from "../images/covernet"
 
 export default function Component() {
   const scrollToGiveClass = () => {
@@ -258,14 +257,14 @@ removeFunction : function(){}
                 <div className="swiper-slide slide1">
                   <div className="seller_item_inner">
                     <a href="#">
-                      <img src={pr1} />
+                      <img src={pr1L} />
                     </a>
                   </div>
                 </div>
                 <div className="swiper-slide slide2">
                   <div className="seller_item_inner">
                     <a href="#">
-                      <img src={pr2} />
+                      <img src={pr2L} />
                     </a>
                   </div>
                 </div>
