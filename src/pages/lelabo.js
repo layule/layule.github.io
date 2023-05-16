e61d20687fe5d48fb40254edbc36f1a2ae17aad1
layule <dbf6655@naver.com>
2023-05-16 19:26:10 +0900

    fix: input up

--- src/pages/lelabo.js
+++ src/pages/lelabo.js
@@ -10,6 +10,15 @@ import { useState } from "react";
 export default function Component() {
   const [activeTab, setActiveTab] = useState(1);
 
+  const [text, setText] = useState("");
+  const onChange = e => {
+    setText(e.target.value);
+  };
+
+  const onReset = () => {
+    setText("");
+  };
+
   return (
     <div className="page-container lelabo">
       <div className="topevent">
@@ -107,15 +116,144 @@ export default function Component() {
       </div>
 
       <footer className="lelabofooter">
-        이엘씨에이한국(유) 대표: Teng, Hsiao-Hua (Catherine), 서울시 강남구 강남대로 382 (역삼동) 메리츠타워, 06232
+        <section className="marketing_bn">
+          <h2>Le Labo Communications</h2>
+          <div className="blook"></div>
+          <div className="blck_line"></div>
+          <div className="listItem">
+            <div className="copy_item">
+              <p>Subscribe to Aesop emails to receive news about products, services, stores, events, cultural interests and more.</p>
+            </div>
+            <div className="certification_box">
+              <input className="certification1" type="checkbox" />
+              <p>I am at least 14 years old (required)</p>
+            </div>
+            <div className="certification_box">
+              <input className="certification2" type="checkbox" />
+              <p>I agree to the collection and use of personal information (required)</p>
+            </div>
+            <div className="letter_box">
+              Information regarding Le Labo's products, services and promotions will be emailed to you for marketing purposes, until you cancel your consent.
+              <br />
+              You do not have to consent to the collection and use of your personal information for marketing purposes, in which case you will not receive marketing information.
+            </div>
+            <div className="certification_box">
+              <input className="certification3" type="checkbox" />
+              <p>I agree to receive marketing information (required)</p>
+            </div>
+            <div className="letter_box">I agree to receive advertising information via newsletter email.</div>
+            <div className="email_input">
+              <div className="letter_input">
+                <input onChange={onChange} value={text} />
+                <button onClick={onReset}>click</button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="footer_gather">
+          <section className="footer_section">
+            <h3>Order & Support</h3>
+            <div className="blook"></div>
+            <nav className="footer_col">
+              <li>
+                <a href="#">contact</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">delivery</a>
+              </li>
+              <li>
+                <a href="#">return</a>
+              </li>
+              <li>
+                <a href="#">Track delivery</a>
+              </li>
+              <li>
+                <a href="#">order history</a>
+              </li>
+              <li>
+                <a href="#">Terms of Use</a>
+              </li>
+            </nav>
+          </section>
+          <section className="footer_section">
+            <h3>Order & Support</h3>
+            <div className="blook"></div>
+            <nav className="footer_col">
+              <li>
+                <a href="#">contact</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">delivery</a>
+              </li>
+              <li>
+                <a href="#">return</a>
+              </li>
+              <li>
+                <a href="#">track delivery</a>
+              </li>
+              <li>
+                <a href="#">order history</a>
+              </li>
+              <li>
+                <a href="#">terms of use</a>
+              </li>
+            </nav>
+          </section>
+          <section className="footer_section">
+            <h3>service</h3>
+            <div className="blook"></div>
+            <nav className="footer_col">
+              <li>
+                <a href="#">corporate purchase</a>
+              </li>
+              <li>
+                <a href="#">chat consultation</a>
+              </li>
+              <li>
+                <a href="#">campaign</a>
+              </li>
+            </nav>
+          </section>
+          <section className="footer_section">
+            <h3>introduction</h3>
+            <div className="blook"></div>
+            <nav className="footer_col">
+              <li>
+                <a href="#">brand story</a>
+              </li>
+              <li>
+                <a href="#">employment</a>
+              </li>
+              <li>
+                <a href="#">privacy policy</a>
+              </li>
+              <li>
+                <a href="#">operation/management policy</a>
+              </li>
+              <li>
+                <a href="#">activation</a>
+              </li>
+              <li>
+                <a href="#">cookie Policy</a>
+              </li>
+            </nav>
+          </section>
+        </div>
+        ELCA Korea Representative: Teng, Hsiao-Hua (Catherine), Meritz Tower, 382 Gangnam-daero (Yeoksam-dong), Gangnam-gu, Seoul, 06232
         <br />
-        사업자등록번호: 211-81-71889
+        Business registration number: 211-81-71889
         <br />
-        통신판매업신고번호: 강남-15737호
+        Mail-order business report number: Gangnam-15737
         <br />
-        고객관리지원팀: 1544-1345 conciergekr@lelabofragrances.com
+        Customer Care Support Team: 1544-1345 conciergekr@lelabofragrances.com
         <br />
-        호스팅제공자: Amazon Web Services
+        hosting provider: Amazon Web Services
         <br />
       </footer>
     </div>
